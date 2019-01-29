dd08babd2f59fec0024d68903c8313866ddfb1cb
Wei9023 <vv880923@gmail.com>
2019-01-29 10:10:38 -0800

    Updated game function and trying to push

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
 'use strict';
 var gameDiv = document.getElementById('rps');
+var rock = document.getElementById('rock');
+var paper = document.getElementById('paper');
+var scissors = document.getElementById('scissors');
+var banner = document.getElementById('banner');
+var choiceArray = [rock, paper, scissors];
 
 function randomChoice (){
-  var random =  Math.floor(Math.random() * (3 - 0) + 0);
-  console.log(random);
+	var random =  choiceArray[Math.floor(Math.random() * choiceArray.length)].alt;
+	console.log(random);
+	return random;
+  
 }
 
 function handleRps (){
-	
+	var userChoice = event.target.alt;
+	console.log(userChoice);
+	var computerChoice = randomChoice();
+}
+
+function win () {
+	banner.textContent= 'You Win!';
+}
+function lose () {
+	banner.textContent= 'You Lose!';
+}
+function gameOver () {
+	banner.textContent= 'GAME OVER!';
 }
 
 
