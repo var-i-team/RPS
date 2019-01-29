14be3b8c592b3abcdb494d5723d46b61bf865eb6
Alexander White <parzivalwhite@gmail.com>
2019-01-29 11:39:57 -0800

    added images and funtion to show results

--- js/app.js
+++ js/app.js
@@ -7,27 +7,38 @@ var banner = document.getElementById('banner');
 var choiceArray = [rock, paper, scissors];
 
 function randomChoice (){
-	var random =  choiceArray[Math.floor(Math.random() * choiceArray.length)].alt;
-	console.log(random);
-	return random;
-  
+  var random =  choiceArray[Math.floor(Math.random() * choiceArray.length)];
+  console.log(random);
+  return random;
 }
 
 function handleRps (){
-	var userChoice = event.target.alt;
-	console.log(userChoice);
-	var computerChoice = randomChoice();
+  var userChoice = event.target;
+  console.log(userChoice);
+  var computerChoice = randomChoice();
+  win();
 }
 
 function win () {
-	banner.textContent= 'You Win!';
+  banner.textContent = 'You Win!';
+  showResults();
 }
 function lose () {
-	banner.textContent= 'You Lose!';
+  banner.textContent = 'You Lose!';
+  showResults();
 }
 function gameOver () {
-	banner.textContent= 'GAME OVER!';
+  banner.textContent = 'GAME OVER!';
+  showResults();
 }
 
+
+function showResults(){
+  var userChoice = event.target;
+  var computerChoice = randomChoice();
+  gameDiv.innerHTML = userChoice, computerChoice;
+  gameDiv.appendChild(userChoice);
+  gameDiv.appendChild(computerChoice);
+}
 
 gameDiv.addEventListener('click', handleRps);
