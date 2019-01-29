68569e8a20e2f7f1c0736bda41b14f3966d348e3
iangifford <lightyagamie10@gmail.com>
2019-01-29 11:37:58 -0800

    adds stable logic and functionality

--- js/app.js
+++ js/app.js
@@ -1,91 +1,4 @@
 'use strict';
-<<<<<<< HEAD
-
-var rps = document.getElementById('rps');
-var rock = document.getElementById('rock');
-var paper = document.getElementById('paper');
-var scissors = document.getElementById('scissors');
-var playerWins = 0;
-var turnCount = 0;
-
-
-
-function handleClick(event){
-    var compChoice = Math.ceil(Math.random(3));
-  if(event.target.id === 'rps'){
-    return;
-  }
-  if(event.target.alt === 'rock'){
-
-
-    if(compChoice === 1){
-
-
-    }
-    if(compChoice === 2){
-      turnCount++;
-      if(turnCount >= 3){
-
-        return;
-      }
-
-    }
-    if(compChoice === 3) {
-      playerWins++;
-      turnCount++;
-      if(turnCount >= 3){
-
-        return;
-      }
-
-    }
-  }
-  if(event.target.alt === 'paper'){
-    if(compChoice === 1){
-      playerWins++;
-      turnCount++;
-        
-      if(turnCount >= 3){
-  
-        return;
-      }
-    }
-    if(compChoice === 2){
-
-    }
-
-  }
-  if(compChoice === 3) {
-    turnCount++;
-    if(turnCount >= 3){
-  
-      return;
-    }
-  }
-  
-  if(event.target.alt === 'scissors'){
-    if(compChoice === 1){
-      turnCount++;
-      if(turnCount >= 3){
-            
-        return;
-      }
-    }
-    if(compChoice === 2){
-      playerWins++;
-      turnCount++;
-        
-      if(turnCount >= 3){
-  
-        return;
-      }
-    }
-    if(compChoice === 3){
-        
-    }
-  }
-}
-=======
 var gameDiv = document.getElementById('rps');
 var rock = document.getElementById('rock');
 var paper = document.getElementById('paper');
@@ -93,29 +6,97 @@ var scissors = document.getElementById('scissors');
 var banner = document.getElementById('banner');
 var choiceArray = [rock, paper, scissors];
 
-function randomChoice (){
-	var random =  choiceArray[Math.floor(Math.random() * choiceArray.length)].alt;
-	console.log(random);
-	return random;
+var playerWins = 0;
+var turnCount = 0;
+
+
+
+function handleClick(event){
+  // var userChoice = event.target.alt;
+  var compChoice =  choiceArray[Math.floor(Math.random() * choiceArray.length)].alt;
+  console.log(compChoice);
+  function win () {
+    banner.textContent= 'You Win!';
+  }
+  function lose () {
+    banner.textContent= 'You Lose!';
+  }
+  function gameOver () {
+    banner.textContent= 'GAME OVER!';
+  }
+  function tie(){
+    banner.textContent = 'Tied, try again!';
+  }
+  if(event.target.id === 'rps'){
+    return;
+  }
+  if(event.target.alt === 'rock'){
+    if(compChoice === 'rock'){
+      return tie();
+
+    }
+    
+    if(compChoice === 'paper'){
+      turnCount++;
+      if(turnCount >= 3){
+        return gameOver();
+      }
+      return lose();
+    }
+    if(compChoice === 'scissors') {
+      playerWins++;
+      turnCount++;
+      if(turnCount >= 3){
+        return gameOver();
+      }
+      return win();
+
+    }
+  }
+  if(event.target.alt === 'paper'){
+    if(compChoice === 'rock'){
+      playerWins++;
+      turnCount++;
+      if(turnCount >= 3){
+        return gameOver();
+      }
+      return win();
+    }
+  }
   
-}
-
-function handleRps (){
-	var userChoice = event.target.alt;
-	console.log(userChoice);
-	var computerChoice = randomChoice();
-}
-
-function win () {
-	banner.textContent= 'You Win!';
-}
-function lose () {
-	banner.textContent= 'You Lose!';
-}
-function gameOver () {
-	banner.textContent= 'GAME OVER!';
-}
+  if(compChoice === 'paper'){
+    return tie();
+  }
 
 
-gameDiv.addEventListener('click', handleRps);
->>>>>>> 0b80fa1c78b405f8ae24b075d8d16a74632c6afa
+  if(compChoice === 'scissors') {
+    turnCount++;
+    if(turnCount >= 3){
+      return lose();
+    }
+  }
+
+  if(event.target.alt === 'scissors'){
+    if(compChoice === 'rock'){
+      turnCount++;
+      if(turnCount >= 3){
+        return lose();
+      }
+    }
+    if(compChoice === 'paper'){
+      playerWins++;
+      turnCount++;
+      if(turnCount >= 3){
+        return gameOver();
+      }
+      return win();
+    }
+
+    if(compChoice === 'scissors'){
+      return tie();
+    }
+  }
+}
+
+gameDiv.addEventListener('click', handleClick);
+
