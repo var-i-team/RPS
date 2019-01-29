2f790dc1264b4e756a602e6c11831edd65bb2ad7
iangifford <lightyagamie10@gmail.com>
2019-01-28 16:28:12 -0800

    completes the nested If statements

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ var turnCount = 0;
 
 
 function handleClick(event){
+    var compChoice = Math.ceil(Math.random(3));
   if(event.target.id === 'rps'){
     return;
   }
   if(event.target.alt === 'rock'){
-    var compChoice = Math.ceil(Math.random(3));
-    // var playerChoice = 1;
+
 
     if(compChoice === 1){
 
@@ -36,27 +36,47 @@ function handleClick(event){
     }
   }
   if(event.target.alt === 'paper'){
-      var compChoice = Math.ceil(Math.random(3));
-      if(compChoice === 1){
-
-
-    }
-    if(compChoice === 2){
-      turnCount++;
-      if(turnCount >= 3){
-
-        return;
-      }
-
-    }
-    if(compChoice === 3) {
+    if(compChoice === 1){
       playerWins++;
       turnCount++;
+        
       if(turnCount >= 3){
-
+  
         return;
       }
+    }
+    if(compChoice === 2){
+
+    }
+
   }
-
+  if(compChoice === 3) {
+    turnCount++;
+    if(turnCount >= 3){
+  
+      return;
+    }
+  }
+  
+  if(event.target.alt === 'scissors'){
+    if(compChoice === 1){
+      turnCount++;
+      if(turnCount >= 3){
+            
+        return;
+      }
+    }
+    if(compChoice === 2){
+      playerWins++;
+      turnCount++;
+        
+      if(turnCount >= 3){
+  
+        return;
+      }
+    }
+    if(compChoice === 3){
+        
+    }
+  }
 }
-
